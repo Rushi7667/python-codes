390ea48960781e0593771f0ab3bcb9b88d2c5f52
Rushi Lunagariya <75974714+Rushi7667@users.noreply.github.com>
2021-07-16 18:27:08 +0530

    added description in the code.

--- number_guass_game.py
+++ number_guass_game.py
@@ -1,3 +1,25 @@
+"""In number guessing game we used if else statements and while loop.
+
+we used random function for system so it can take any random integer.
+
+we take math function for calculate the log.
+
+also we have takken log base 2 for enough chances for users.
+
+->How it works?
+
+When user give the lower and upper value to the system...
+
+System takes random integers from given range and gives chance to the user according to the range.
+
+now user enter the value between that range and system check that the given value maches or not. if it is higher or lower than system pradict then it will shows, And if user lose all chance to guess than system reveals the value.
+
+"""
+
+
+# Code:
+
+
 import math
 import random
 
